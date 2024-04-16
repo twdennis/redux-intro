b918f89fb99ea333b8c197b1d098a3700ebb09ac
Tom <131278560+twdennis@users.noreply.github.com>
2024-04-16 09:52:13 +0100

    trying again

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -11,6 +11,7 @@ function AccountOperations() {
   const [currency, setCurrency] = useState("USD");
 
   const dispatch = useDispatch();
+
   const {
     loan: currentLoan,
     loanPurpose: currentPurpose,
